1883220c86efe698eb3bc68be3ab481c2ca8c3bf
samoelsamu07-afk <samoelsamu07@gmail.com>
2026-03-11 20:09:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log("Bot online!");
+});
+
+client.login(process.env.TOKEN);
